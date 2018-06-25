e5f7bfb18e604ac27048f39b0ecc23ce653d1841
rookiengineer <rookiengineer@gmail.com>
2018-06-25 04:51:24 +0000

    second commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,4 +20,9 @@ class HomeController < ApplicationController
         @lunch = menu.sample
         render 'home/lunch'
     end
+    
+    def search
+        # form을 통해 검색어를 입력받아
+        # naver, daum, google에 넘겨준다
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'home#index' #컨트롤러의 이름을 홈이라고 했어, #는 인덱스 액션
-  get '/lotto' => 'home#lotto'
+  get '/home/lotto'
   get '/lunch' => 'home#lunch'
+  get '/search' => 'home#search'
 end
